f07822bfd3c56a1bd92e64f023419b192c2c9b8f
Binaya Panta <binaya.panta@compliancequest.com>
2022-12-21 16:51:09 +1100

    Updated Code

--- src/App.js
+++ src/App.js
@@ -1,22 +1,14 @@
 import logo from './logo.svg';
+import benzLogo from './logo_benz.jpg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <img src={benzLogo} className="App-logo" alt="logo" />
+        <p> Benz IT Services </p>
+        <p> Website Coming Soon </p>
       </header>
     </div>
   );
