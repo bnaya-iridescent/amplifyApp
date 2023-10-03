377e43ea8337c9eb35b989f49b43a412da7d1c2f
Binaya Panta <binaya.panta@compliancequest.com>
2023-10-03 18:37:36 +1100

    contact info and services added

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import logo from './logo.svg';
+import React from 'react';
 import benzLogo from './logo_benz.jpg';
 import './App.css';
 
@@ -7,8 +7,29 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={benzLogo} className="App-logo" alt="logo" />
-        <p> Benz IT Services </p>
-        <p> Website Coming Soon </p>
+        <h1>Benz IT Services</h1>
+        <p>Website Coming Soon</p>
+
+        {/* Contact Information */}
+        <div className="contact-info">
+          <h2>Contact Us</h2>
+          <p>Email: info@benzitservices.com</p>
+          <p>Phone: +61 (466) 228-870</p>
+          <p>Address: 6 Philip Ave, Montrose TAS</p>
+        </div>
+
+        {/* Services Offered */}
+        <div className="services">
+          <h2>Our Services</h2>
+          <ul>
+            <li>Salesforce Development</li>
+            <li>Web Development</li>
+            <li>Mobile App Development</li>
+            <li>IT Consulting</li>
+            <li>Cloud Services</li>
+            {/* Add more services as needed */}
+          </ul>
+        </div>
       </header>
     </div>
   );
